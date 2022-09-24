663d91e41193f69f1b0a02b614ac8417a2fd0654
akrem-cpu <akremabdu71@gmail.com>
2022-09-24 18:36:48 -0500

    Initial commit

---
+++ src/main/java/com/akrem/bean_annotation/App.java
@@ -0,0 +1,18 @@
+package com.akrem.bean_annotation;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+
+    public static void main(String[] args) {
+        ApplicationContext container = new AnnotationConfigApplicationContext(ConfigApp.class,ConfigAny.class);
+
+       container.getBean(FullTimeMentor.class).createAccount();
+        System.out.println(container.getBean(String.class));
+
+
+    }
+}
---
+++ src/main/java/com/akrem/bean_annotation/FullTimeMentor.java
@@ -0,0 +1,10 @@
+package com.akrem.bean_annotation;
+
+public class FullTimeMentor {
+
+
+    public void  createAccount(){
+        System.out.println("Full time mentor is created");
+    }
+
+}
